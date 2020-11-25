b7f378d22ad0c015556f0a6f249477765afa1173
Elias Avilez <eliasramos159@gmail.com>
2020-11-24 23:05:10 -0500

    add private ip en conexion mysql

--- src/db/db.py
+++ src/db/db.py
@@ -6,7 +6,7 @@ class ConexionDB:
     @staticmethod
     def conexion():
         cnx = mysql.MySQLConnection(
-            host="34.121.18.138",
+            host="10.1.112.11",
             port=3306,
             user="elias",
             password="elias",
